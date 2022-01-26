378880e09df92c37cbeaeb69131f80d42974ac96
ss1870 <ss1870@bristol.ac.uk>
2022-01-26 15:12:05 +1300

    Tidy up main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-
 from source.classes import LiftingSurface
 
-wing1 = LiftingSurface(250, 80, 800, -200, 3, 75, 15)
-wing1.generate_coords(npts=101)
-# wing1.plot2D()
-wing1.plot3D()
-print("Simple wing area =", str(wing1.calc_simple_proj_wing_area()))
-print("Trapz wing area =", str(wing1.calc_trapz_proj_wing_area()))
-print("Aspect ratio =", str(wing1.calc_AR()))
-print("Wing lift =", str(wing1.calc_lift(V=5, aoa=4, rho=1025)), "Newtons")
+front_wing = LiftingSurface(rt_chord=250,
+                            tip_chord=80,
+                            span=800,
+                            sweep_tip=-200,
+                            sweep_curv=3,
+                            dih_tip=75,
+                            dih_curve=15)
 
-# wing1.generate_LL_geom(10)
-# wing1.LL_seg_area
-# print(wing1.BVs[39].node1, wing1.BVs[39].node2, wing1.BVs[0].elmtID, wing1.BVs[0].circ)
-# print(len(wing1.BVs))
-# print(wing1.BVs[-1].elmtID)
+front_wing.generate_coords(npts=101)
+front_wing.plot2D()
+front_wing.plot3D()
+print("Simple wing area =", str(front_wing.calc_simple_proj_wing_area()))
+print("Trapz wing area =", str(front_wing.calc_trapz_proj_wing_area()))
+print("Aspect ratio =", str(front_wing.calc_AR()))
+print("Wing lift =", str(front_wing.calc_lift(V=5, aoa=4, rho=1025)), "Newtons")
+
+front_wing.generate_LL_geom(10)
+print("Lifting line wing area =", str(front_wing.LL_seg_area))
+
+# print(front_wing.BVs[39].node1, front_wing.BVs[39].node2, front_wing.BVs[0].elmtID, front_wing.BVs[0].circ)
+# print(len(front_wing.BVs))
+# print(front_wing.BVs[-1].elmtID)
